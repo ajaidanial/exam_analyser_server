230d73f14562c1c693a75534255ee0569d42880d
ajaidanial <ajai@geopits.com>
2020-10-01 03:01:00 +0530

    added the subject model

--- exam_analyser/examination/models.py
+++ exam_analyser/examination/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Subject(models.Model):
+    """
+    Model that holds the subject name data. This is linked to the teacher and the student
+    and is used in mapping the exams dynamically.
+    """
+
+    name = models.CharField(max_length=255)
