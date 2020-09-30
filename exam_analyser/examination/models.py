ca7c4b41f6e0d72cfa82c81d3deeb4d564a30745
ajaidanial <ajai@geopits.com>
2020-10-01 03:56:34 +0530

    added the exam model

--- exam_analyser/examination/models.py
+++ exam_analyser/examination/models.py
@@ -10,3 +10,16 @@ class Subject(BaseModel):
     """
 
     name = models.CharField(max_length=255, unique=True)
+
+
+class Exam(BaseModel):
+    """
+    Model that holds the exam name data. This is linked to the question papers. This is mostly
+    a read only data for now. This contains the values:
+        1. Quarterly Exam
+        2. Half Yearly Exam
+        3. Annual Exam
+    This might get dynamic in the future.
+    """
+
+    name = models.CharField(max_length=255, unique=True)
