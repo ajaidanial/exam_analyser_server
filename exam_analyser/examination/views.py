ad31e267e71d06074269ab37ccd3ac9475828acd
ajaidanial <ajai@geopits.com>
2020-11-02 18:10:06 +0530

    added everything

--- exam_analyser/examination/views.py
+++ exam_analyser/examination/views.py
@@ -59,8 +59,46 @@ class ExamSubjectOverviewView(APIView):
     This view displays the data for the exam, subject and question paper view.
     This gives the data for the table like view through which the question paper can be viewed
     or created. This will include the exam, subject and question paper.
+
+    Output Schema: [
+        {
+            ...
+            "linked_subjects": [
+                {
+                    ...
+                    "linked_question_paper": {} || null
+                }
+                ... subjects data
+            ]
+        }
+        ...exams data
+    ]
     """
 
     def get(self, request, **kwargs):
-        overview_data = {}
+        overview_data = ExamSerializer(
+            Exam.objects.all(), context=self.get_serializer_context(), many=True
+        ).data
+
+        for exam_data in overview_data:
+            exam_id = exam_data["id"]
+            exam_data["linked_subjects"] = SubjectSerializer(
+                Subject.objects.all(), context=self.get_serializer_context(), many=True
+            ).data
+            for subject_data in exam_data["linked_subjects"]:
+                subject_id = subject_data["id"]
+                question_paper_qs = QuestionPaper.objects.filter(
+                    exam=exam_id, subject=subject_id
+                )
+                subject_data["linked_question_paper"] = (
+                    QuestionPaperSerializer(
+                        question_paper_qs.first(), context=self.get_serializer_context()
+                    ).data
+                    if question_paper_qs.exists()
+                    else None
+                )
+
         return Response(data=overview_data, status=status.HTTP_200_OK)
+
+    def get_serializer_context(self):
+        return {"view": self, "request": self.request}
